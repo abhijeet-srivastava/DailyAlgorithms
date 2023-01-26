fbba035d83a15a7b4b705329b647a37a3fa69feb
Abhijeet Srivastva <malviyanabhijeet@gmail.com>
2023-01-27 00:07:21 +0530

    Added 26-Jan LC question

--- src/main/java/com/flipkart/FlipKartTest.java
+++ src/main/java/com/flipkart/FlipKartTest.java
@@ -256,4 +256,20 @@ public class FlipKartTest {
         }
         return -1;
     }
+    public int findCheapestPrice1(int n, int[][] flights, int src, int dst, int k) {
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
+        for(int i = 0; i <= k; i++) {
+            int[] tmp = Arrays.copyOf(dist, n);
+            for(int[] flight: flights) {
+                if(dist[flight[0]] == Integer.MAX_VALUE) {
+                    continue;
+                }
+                tmp[flight[1]] = Math.min(tmp[flight[1]], dist[flight[0]] + flight[1]);
+            }
+            dist = tmp;
+        }
+        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
+    }
 }
