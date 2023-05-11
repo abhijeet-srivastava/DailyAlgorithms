a293f7dc628ec9f48a42ff86eb00ba78ad58d03c
abhijeet.srivastva <malviyanabhijeet@gmail.com>
2023-05-11 09:34:24 +0530

    Adding 11th May challenge

--- src/main/java/com/leetcode/mothly/may23/DailyCodeMay.java
+++ src/main/java/com/leetcode/mothly/may23/DailyCodeMay.java
@@ -120,5 +120,19 @@ public class DailyCodeMay {
         }
         return sum;
     }
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int l1 = nums1.length, l2 = nums2.length;
+        int[][] DP = new int[l1+1][l2+1];
+        for(int i = 1; i <= l1; i++) {
+            for(int j = 1; j <= l2; j++) {
+                if(nums1[i-1] == nums2[j-1]) {
+                    DP[i][j] = 1 + DP[i-1][j-1];
+                } else {
+                    DP[i][j] = Math.max(DP[i-1][j], DP[i][j-1]);
+                }
+            }
+        }
+        return DP[l1][l2];
+    }
 
 }
