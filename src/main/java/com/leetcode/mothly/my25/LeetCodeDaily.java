fc1de1fe68e548b239c79d0caa782865ba9fdb9f
abhijeet.srivastva <malviyanabhijeet@gmail.com>
2025-05-16 13:38:49 +0530

    Add lc daily 16 may

--- src/main/java/com/leetcode/mothly/my25/LeetCodeDaily.java
+++ src/main/java/com/leetcode/mothly/my25/LeetCodeDaily.java
@@ -1,9 +1,77 @@
 package com.leetcode.mothly.my25;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.PriorityQueue;
 
 public class LeetCodeDaily {
+
+    public static void main(String[] args) {
+        LeetCodeDaily lcd = new LeetCodeDaily();
+        lcd.test16May();
+    }
+
+    private void test16May() {
+        String[] words = {"bab","dab","cab"};
+        int[] groups = {1,2,2};
+        List<String> res = getWordsInLongestSubsequence(words, groups);
+        res.stream().forEach(System.out::println);
+    }
+
+
+    public List<String> getWordsInLongestSubsequence(String[] words, int[] groups) {
+        int len = words.length;
+        WordGroup[] DP = new WordGroup[len];
+        for(int i = 0; i < len; i++) {
+            DP[i] = new WordGroup(words[i], groups[i], 1, -1);
+        }
+        int maxIdx = 0;
+        for(int i = 1; i < len; i++) {
+            int maxLenIdx = 1;
+            for(int j = i-1; j >= 0; j--) {
+                if(DP[i].group != DP[j].group
+                        && hammingDistance(DP[i].word, DP[j].word) == 1
+                        && DP[i].len < (1+DP[j].len)) {
+                    DP[i] = new WordGroup(words[i], groups[i], 1+DP[j].len, j);
+                }
+            }
+            if(DP[i].len > DP[maxIdx].len) {
+                maxIdx = i;
+            }
+        }
+        List<String> res = new LinkedList<>();
+        while(maxIdx >= 0) {
+            res.add(0, DP[maxIdx].word);
+            maxIdx =  DP[maxIdx].prev;
+        }
+        return res;
+    }
+
+    private int hammingDistance(String w1, String w2) {
+        if(w1.length() != w2.length()) {
+            return 2;
+        }
+        int dist = 0;
+        for(int idx = 0; idx < w1.length();idx++) {
+            if(w1.charAt(idx) != w2.charAt(idx)) {
+                dist += 1;
+            }
+        }
+        return dist;
+    }
+    private class WordGroup {
+        String word;
+        int group;
+        int len;
+        int prev;
+        public WordGroup(String word, int group, int len, int prev) {
+            this.word = word;
+            this.group = group;
+            this.len = len;
+            this.prev = prev;
+        }
+    }
 
     public int minTimeToReach(int[][] moveTime) {
         int m = moveTime.length, n = moveTime[0].length;
